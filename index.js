512ae33b5eaf3f4484b71b43d1f83eceda8be0ca
ashwani <vscashwani@gmail.com>
2024-04-09 00:30:30 +0530

    create a server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("i am working")
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Enable all CORS requests
+app.use(cors());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Server is working!!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
